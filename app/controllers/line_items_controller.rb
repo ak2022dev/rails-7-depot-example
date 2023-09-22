555a5398e153e249e08f9a934dc7270cdd5a89fe
Amir Khan <ak2022dev@gmail.com>
2023-09-22 16:32:43 +0100

    Refactor modify create method in Line Items controller to use params

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -23,7 +23,8 @@ class LineItemsController < ApplicationController
 
   # POST /line_items or /line_items.json
   def create
-    @line_item = LineItem.new(line_item_params)
+    product = Product.find(params[:product_id])  
+    @line_item = @cart.line_items.build(product: product)
 
     respond_to do |format|
       if @line_item.save
