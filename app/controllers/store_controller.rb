7a33657dc53263a6fd81cfd7bc7a13ae90cf4c87
Amir Khan <ak2022dev@gmail.com>
2023-09-24 11:21:14 +0100

    WIP attempt to count number of times store index is accessed by using instance variable in StoreController. Untested

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,7 @@
 class StoreController < ApplicationController
   def index
+    @store_index_access_count ||= 0
+    @store_index_access_count += 1
     @products = Product.order(:title)
   end
 end
