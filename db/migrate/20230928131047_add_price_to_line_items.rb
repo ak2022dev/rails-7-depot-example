da9c42905f3c482313170338ad8de8a98e06f520
Amir Khan <ak2022dev@gmail.com>
2023-09-28 14:13:33 +0100

    Refactor generated migration to add price to line items, setting default to 0.0

--- db/migrate/20230928131047_add_price_to_line_items.rb
+++ db/migrate/20230928131047_add_price_to_line_items.rb
@@ -1,5 +1,5 @@
 class AddPriceToLineItems < ActiveRecord::Migration[7.0]
   def change
-    add_column :line_items, :price, :decimal
+    add_column :line_items, :price, :decimal, default 0.0
   end
 end
