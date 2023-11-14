3e41a61b05ef59bb4974ba605a9963077a073e3d
Amir Khan <ak2022dev@gmail.com>
2023-11-14 12:13:11 +0000

    Fix routes by removing duplicates and re-ordering

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # Defines the root path route ("/")
+  root 'store#index', as: 'store_index'
+  resources :products
   resources :line_items
   resources :carts
-  root 'store#index', as: 'store_index'
-  get 'store/index'
-  resources :products
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
